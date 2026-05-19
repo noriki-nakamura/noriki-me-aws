14cda3a64c2612dcf2c6a89c77fc5c61f4f1143a
Noriki Nakamura <noriki.nakamura.0424@gmail.com>
2026-05-19 13:22:32 +0900

    feat: OIDC ベースの Github Actions 用 IAM ロールを追加

---
+++ github-actions/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    key          = "github-actions/terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
